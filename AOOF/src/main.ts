e482d01ad5649e6b3e58ef1eabdd59a08afad393
Adesh <adeshdance@gmail.com>
2024-04-30 09:55:34 +0530

    Created Admin dashboard page. Added chart.js for data visualization.

--- AOOF/src/main.ts
+++ AOOF/src/main.ts
@@ -3,12 +3,24 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import PrimeVue from "primevue/config";
+import AutoComplete from 'primevue/autocomplete';
+import Chart from 'primevue/chart';
+
 import App from './App.vue'
 import router from './router'
 
+import "primeflex/primeflex.css";
+import "primevue/resources/themes/aura-light-green/theme.css";
+import "primeicons/primeicons.css";
+
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
+app.use(PrimeVue, { ripple: true  });
+
+app.component('AutoComplete', AutoComplete);
+app.component('Chart', Chart);
 
 app.mount('#app')
--- AOOF/src/router/index.ts
+++ AOOF/src/router/index.ts
@@ -5,6 +5,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 //Components
 
 import landingPage from '@/views/landingPage/landingPage.vue'
+import adminPage from '@/views/adminPage/adminPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +14,11 @@ const router = createRouter({
       path: "/",
       name: "Login",
       component: landingPage
+    },
+    {
+      path: "/admin",
+      name: "Admin",
+      component: adminPage
     }
   ]
 })
