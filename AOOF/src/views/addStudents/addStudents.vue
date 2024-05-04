29845f0fc9de25bc72c386df9497abaa5ad9fed3
Adesh <adeshdance@gmail.com>
2024-05-04 12:49:46 +0530

    Completed Add Students modal.

--- AOOF/src/views/addStudents/addStudents.vue
+++ AOOF/src/views/addStudents/addStudents.vue
@@ -1,9 +1,9 @@
 <script lang="ts">
 //dependencies
 
-import { ref, defineComponent } from 'vue';
+import { ref, nextTick } from 'vue';
 import { useRouter } from 'vue-router';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 
 //styles
 
@@ -31,7 +31,7 @@ interface Student {
 
 const departments = ref<Department[]>([]);
 const visibleAddStudentModal = ref<boolean[]>([]);
-const addStudentsMethod = ref<string>('csv');
+const addStudentsMethod = ref<string>('mail');
 const students = ref<Student[]>([]);
 const newStudent = ref<Student>({
   name: '',
@@ -39,6 +39,8 @@ const newStudent = ref<Student>({
   email: '',
 });
 const currentDepartmentIndex = ref<number | null>(null);
+const fileName = ref<string>('Not selected file');
+const fileCheck = ref<boolean>(false);
 
 //functions
 
@@ -111,9 +113,90 @@ const saveStudentsByMail = (index: number) => {
     students.value = [];
     visibleAddStudentModal.value[currentDepartmentIndex.value] = false;
     const departmentJson = JSON.stringify(departments.value[currentDepartmentIndex.value]);
-    console.log(departmentJson);
   }
 };
+
+const uploadStudents = (event: Event) => {
+  const inputElement = event.target as HTMLInputElement;
+  if (inputElement.files && inputElement.files.length > 0) {
+    const file = inputElement.files[0];
+    fileName.value = file.name; 
+    fileCheck.value = true;
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      if (e.target) {
+        const contents = e.target.result;
+        if (typeof contents === 'string') {
+          const lines = contents.split('\n');
+          for (let i = 1; i < lines.length; i++) {
+            if (lines[i].trim() !== '') { // Check if the line is not empty
+              const [name, roll, email] = lines[i].split(',');
+              const isDuplicate = students.value.some(
+                (student) =>
+                  student.roll === roll ||
+                  student.email === email
+              );
+              if (!isDuplicate) {
+                students.value.push({ name, roll, email });
+              }
+            }
+          }
+        }
+      }
+    };
+    reader.readAsText(file);
+  }
+};
+
+const handleDrop = (event: DragEvent) => {
+  event.preventDefault();
+  event.stopPropagation();
+  if (event.dataTransfer && event.dataTransfer.files.length > 0) {
+    const file = event.dataTransfer.files[0];
+    if (file.name.endsWith('.csv')) {
+      fileName.value = file.name;
+      fileCheck.value = true;
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        if (e.target) {
+          const contents = e.target.result;
+          if (typeof contents === 'string') {
+            const lines = contents.split('\n');
+            for (let i = 1; i < lines.length; i++) {
+              if (lines[i].trim() !== '') { // Check if the line is not empty
+                const [name, roll, email] = lines[i].split(',');
+                const isDuplicate = students.value.some(
+                  (student) =>
+                    student.roll === roll ||
+                    student.email === email
+                );
+                if (!isDuplicate) {
+                  students.value.push({ name, roll, email });
+                }
+              }
+            }
+          }
+        }
+      };
+      reader.readAsText(file);
+    } else {
+      window.alert('Please upload a CSV file');
+    }
+  }
+};
+
+const downloadTemplate = () => {
+  const csvContent = "name,roll,email\n";
+  const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.setAttribute("href", url);
+  link.setAttribute("download", "template.csv");
+  link.style.visibility = 'hidden';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
 
 //export
 
@@ -122,6 +205,12 @@ export default {
     const router = useRouter();
 
     const navigateBack = () => {
+      router.go(-1);
+    }
+
+    const saveDepartment = () => {
+      const departmentJson = JSON.stringify(departments.value);
+      console.log(departmentJson);
       router.go(-1);
     }
 
@@ -138,6 +227,12 @@ export default {
       addStudents,
       deleteStudent,
       saveStudentsByMail,
+      uploadStudents,
+      fileName,
+      handleDrop,
+      fileCheck,
+      downloadTemplate,
+      saveDepartment,
     };
   }
 }
@@ -270,10 +365,65 @@ export default {
 
                   <!-- Add Students By CSV -->
 
-                  <div v-else></div>
+                  <div v-else>
+
+                    <!-- Download CSV Template -->
+
+                    <div :class="styles.addStudents_byCSV_download_container">
+                      <button @click="downloadTemplate">
+                        <i class="pi pi-download" />
+                        Download Template
+                      </button>
+                    </div>
+
+                    <!-- Upload CSV -->
+                    <div :class="styles.addStudents_byCSV_container">
+
+                      <!-- Input Container -->
+
+                      <div :class="styles.container"> 
+                        <div :class="styles.header"
+                          @dragover.prevent
+                          @drop="handleDrop"
+                        > 
+                          <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier"> 
+                              <path d="M7 10V9C7 6.23858 9.23858 4 12 4C14.7614 4 17 6.23858 17 9V10C19.2091 10 21 11.7909 21 14C21 15.4806 20.1956 16.8084 19 17.5M7 10C4.79086 10 3 11.7909 3 14C3 15.4806 3.8044 16.8084 5 17.5M7 10C7.43285 10 7.84965 10.0688 8.24006 10.1959M12 12V21M12 12L15 15M12 12L9 15" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> 
+                            </g>
+                          </svg> 
+                          <p v-if="fileCheck">{{ fileName }}</p>
+                          <p v-else>Drag and Drop to Upload!</p>
+                        </div> 
+                        <label for="file" :class="styles.footer"> 
+                          <svg fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M15.331 6H8.5v20h15V14.154h-8.169z"></path><path d="M18.153 6h-.009v5.342H23.5v-.002z"></path></g></svg> 
+                          <p>{{ fileName }}</p> 
+                          <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M5.16565 10.1534C5.07629 8.99181 5.99473 8 7.15975 8H16.8402C18.0053 8 18.9237 8.9918 18.8344 10.1534L18.142 19.1534C18.0619 20.1954 17.193 21 16.1479 21H7.85206C6.80699 21 5.93811 20.1954 5.85795 19.1534L5.16565 10.1534Z" stroke="#000000" stroke-width="2"></path> <path d="M19.5 5H4.5" stroke="#000000" stroke-width="2" stroke-linecap="round"></path> <path d="M10 3C10 2.44772 10.4477 2 11 2H13C13.5523 2 14 2.44772 14 3V5H10V3Z" stroke="#000000" stroke-width="2"></path> </g></svg>
+                        </label> 
+                        <input id="file" type="file" accept=".csv" :style="{ display: 'none' }" @change="uploadStudents">
+                      </div>
+
+                    </div>
+
+                    <!-- Save Button -->
+
+                    <div :class="styles.addStudents_modal_button_container">
+                      <button @click="saveStudentsByMail(index)">
+                        <i class="pi pi-save" />
+                        Save
+                      </button>
+                    </div>
+
+                  </div>
 
                 </div>
+
               </Dialog>
+
+              <i class="pi pi-eye" />
+
+              <!-- View Students Modal -->
 
             </div>
         </div>
@@ -282,7 +432,7 @@ export default {
     <!-- Footer -->
 
     <div :class="styles.addStudents_footer">
-        <button>
+        <button @click="saveDepartment">
             <i class="pi pi-save" />
             Save
         </button>
